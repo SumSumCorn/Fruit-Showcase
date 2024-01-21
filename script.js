e65baeedb8daaa4a8a0a58fd7add137533b36e68
sumsumcorn <nicecorntea@naver.com>
2024-01-21 19:42:00 +0900

    fix: 불필요한 코드,주석 삭제

--- script.js
+++ script.js
@@ -18,23 +18,12 @@ fruitNames.forEach((fruit) => {
   fruits[fruit] = 'assets/' + fruit + '.png';
 });
 
-// console.log(fruits);
-
 function changeFruit(fruit) {
   const fruitImage = fruits[fruit];
   const mainFruit = document.getElementById('main-fruit');
   mainFruit.src = fruitImage;
 
   mainFruit.style.animationName = '';
-  // todo: 에니메이션 제작필요
-  /*
-  setTimeout(() => {
-    mainFruit.style.animationName = 'rollIn';
-    setTimeout(() => {
-      mainFruit.style.animationName = '';
-    }, 2000); // Reset animation after 2 seconds
-  });
-  */
 }
 
 const audio = document.getElementById('myAudio');
@@ -48,25 +37,3 @@ function toggleAudio() {
     audio.pause();
   }
 }
-
-// // Additional CSS for rolling animation
-// document.styleSheets[0].insertRule(
-//   `
-//     @keyframes rollIn {
-//         0% { transform: translateX(-100%); }
-//         100% { transform: translateX(0); }
-//     }
-// `,
-//   0
-// );
-
-// const gasp = require('gasp')
-// const background = document.querySelector('#main-fruit');
-
-// gsap.to(background, {
-//   duration: 2,
-//   // x: 100,
-//   borderRadius: 100,
-//   rotation: 360,
-//   repeat: -1,
-// });
